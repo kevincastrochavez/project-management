f844cd4568a56b239b9bd751cba8df0d434caa8c
Kevin <kevin.castro.chavez0@gmail.com>
2022-11-03 13:29:27 -0600

    setup the intro screen and move to it after 2.5 seconds

--- app/src/main/java/com/example/projectmanagement/SplashActivity.kt
+++ app/src/main/java/com/example/projectmanagement/SplashActivity.kt
@@ -1,7 +1,9 @@
 package com.example.projectmanagement
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.Window
 import android.view.WindowManager
 
@@ -11,5 +13,12 @@ class SplashActivity : AppCompatActivity() {
         setContentView(R.layout.activity_splash)
 
         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
+        Handler().postDelayed({
+            // Sends from current activity to IntroActivity
+            startActivity(Intent(this@SplashActivity, IntroActivity::class.java))
+            // Finish current activity
+            finish()
+        }, 2500)
     }
 }
