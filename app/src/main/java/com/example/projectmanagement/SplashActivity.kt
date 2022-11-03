c232c832e8df3f6c11583791cbd76a2245df6617
Kevin <kevin.castro.chavez0@gmail.com>
2022-11-03 12:32:30 -0600

    setup splashscreen and background

---
+++ app/src/main/java/com/example/projectmanagement/SplashActivity.kt
@@ -0,0 +1,15 @@
+package com.example.projectmanagement
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+    }
+}
